78d75924234de05bdf60ac7d91025b1d06c6d598
Vince <vince@vinces-imac.local>
2018-10-29 13:18:16 -0700

    First Commit test with a sample tab project

---
+++ OvercomingMS/OvercomingMS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OvercomingMS
+//
+//  Created by Vince on 10/29/18.
+//  Copyright © 2018 DrexelOvercomingMS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ OvercomingMS/OvercomingMS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OvercomingMS
+//
+//  Created by Vince on 10/29/18.
+//  Copyright © 2018 DrexelOvercomingMS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
